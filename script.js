a5294e5f04d3a3ecd136296f587af00194f4a5fb
Aman Kumar <125535132+Aman13052002@users.noreply.github.com>
2026-05-29 01:34:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
-
+const list = document.getElementById('infi-list');
+function addItems(count){
+	const currLength = list.children.length;
+	for(let i=1;i<=count;i++){
+		const li = document.createElement('li');
+		li.textContent(`List Item ${currLength + i}`);
+		list.appendChild(li);
+	}
+}
+addItems(10);
+window.addEventListener("scroll", function () {
+  const scrollTop = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const documentHeight =
+    document.documentElement.scrollHeight;
+  if (scrollTop + windowHeight >= documentHeight) {
+    addItems(2);
+  }
+});
